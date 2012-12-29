13907007e323d76d1b2c8459e1033cb356e0c2ae
Daniel Woffinden <daw10@ic.ac.uk>
2012-12-29 01:59:19 +0100

    added a scan button

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -3,16 +3,52 @@ package com.backrow.wps
 import collection.JavaConversions._
 
 import android.app.Activity
-import android.os.Bundle
-import android.net.wifi.WifiManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity extends Activity with TypedActivity {
+
+  val TAG = "WifiPos"
+
+  val resultsAvailable = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+
+  lazy val wm = getSystemService(Context.WIFI_SERVICE).asInstanceOf[WifiManager]
+
+  val wifiReceiver = new BroadcastReceiver() {
+    def onReceive(c: Context, i: Intent) {
+      findView(TR.textview).setText(
+        "Results:\n" + wm.getScanResults.view.map(r => Seq(r.BSSID, ": ", r.level).mkString).mkString("\n")
+      )
+    }
+  }
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-		val wm = getSystemService(Context.WIFI_SERVICE).asInstanceOf[WifiManager]
-    findView(TR.textview).setText(wm.getScanResults.view.flatMap(r => Seq(r.BSSID, ": ", r.level, ", ")).mkString)
+    findView(TR.buttonscan).setOnClickListener(new View.OnClickListener() {
+      def onClick(p1: View) {
+        findView(TR.textview).setText("Starting Scan...")
+        wm.startScan()
+      }
+    })
+
+    registerReceiver(wifiReceiver, resultsAvailable)
+  }
+
+  override def onPause() {
+    unregisterReceiver(wifiReceiver)
+    super.onPause()
+  }
+
+  override def onResume() {
+    registerReceiver(wifiReceiver, resultsAvailable)
+    super.onResume()
   }
 }
